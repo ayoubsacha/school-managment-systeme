059f667a6eea3e4046d81115d6438fae426d5a83
sacha ayoub <ayoub.sacha@etu.uae.ac.ma>
2026-04-01 00:33:06 +0100

    Vue index et dashboard dans faculty/views.py

--- school/faculty/views.py
+++ school/faculty/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# faculty/views.py
+from django.shortcuts import render, redirect
 
-# Create your views here.
 def index(request):
-    return render(request, 'Home/index.html')
+    return render(request, 'authentication/login.html')
+
+def dashboard(request):
+    return render(request, 'students/student-dashboard.html')
